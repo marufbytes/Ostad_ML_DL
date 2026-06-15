e7f499407f9abfe5cfb31303cffee576e0b13a66
marufbytes <marufahammed215@gmail.com>
2026-06-15 22:56:26 +0600

    Set

---
+++ Module_2_DS/set.py
@@ -0,0 +1,15 @@
+fruits = {"mango","apple","banana","banana","apple","jackfruit"}
+for fruit in fruits:
+    print(fruit)     #Unordered
+print("\n")
+print(fruits)        #only uniques item
+print("\n")
+
+
+num=[10,20,30,40,10,30]
+num_set=set(num)
+
+print(num)
+print(num_set)
+
+
